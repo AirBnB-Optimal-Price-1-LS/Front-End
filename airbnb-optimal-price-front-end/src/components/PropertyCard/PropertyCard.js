dc943b4b9652c8234866b186e2b98f7d3efc11f6
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2019-11-21 15:45:13 -0500

    added images to cards

--- airbnb-optimal-price-front-end/src/components/PropertyCard/PropertyCard.js
+++ airbnb-optimal-price-front-end/src/components/PropertyCard/PropertyCard.js
@@ -3,6 +3,19 @@ import { axiosWithAuth } from "../../axiosWithAuth/axiosWithAuth";
 import { UserContext } from "../../contexts/UserContext";
 import "./Card.css";
 
+import Treptow from '../../images/treptow.jpg';
+import Lichtenberg from '../../images/lichtenberg.jpg';
+import Charlottenburg from '../../images/charlottenburg.jpg';
+import Spandau from '../../images/spandau.jpg';
+import Neukolln from '../../images/neukolln.jpg';
+import Mitte from '../../images/mitte.jpg';
+import Friedrichshain from '../../images/friedrichshain.jpg';
+import Pankow from '../../images/pankow.jpg';
+import Steglitz from '../../images/steglitz.jpg';
+import Marzahn  from '../../images/Marzahn.jpg';
+import Schoeneberg  from '../../images/schoeneberg.jpg';
+
+
 const PropertyCard = props => {
   const { userProperty, setUserProperty } = useContext(UserContext);
   // console.log("IN CARD:", props);
@@ -11,8 +24,6 @@ const PropertyCard = props => {
     e.preventDefault();
     props.history.push(`/Dashboard/EditListing/${props.property.id}`);
   };
-
-  // console.log(props.property.id);
 
   const deleteProperty = e => {
     e.preventDefault();
@@ -29,22 +40,58 @@ const PropertyCard = props => {
       .catch(err => console.log(err));
   };
 
+  const setName = name => {
+    if(
+      name === 'Lichtenberg' ||
+      name === 'Neukölln' ||
+      name === 'Pankow' ||
+      name === 'Mitte' ||
+      name === 'Reinickendorf' ||
+      name === 'Treptow-Köpenick' ||
+      name === 'Spandau'
+      ) {
+        return <>{name}<br/>&nbsp;</>
+    } else {
+      return name;
+    }
+  }
+
+  const setImage = name => {
+    switch(name) {
+      case 'Treptow-Köpenick':
+        return Treptow;
+      case 'Lichtenberg':
+        return Lichtenberg;
+      case 'Charlottenburg-Wilm.':
+        return Charlottenburg;
+      case 'Mitte':
+        return Mitte;
+      case 'Tempelhof-Schöneberg':
+        return Schoeneberg;
+      case 'Spandau':
+        return Spandau;
+      case 'Marzahn-Hellersdorf':
+        return Marzahn;
+      case 'Pankow':
+        return Pankow;
+      case 'Neukölln':
+        return Neukolln;
+      case 'Steglitz-Zehlendorf':
+        return Steglitz;
+      case 'Friedrichshain-Kreuzberg':
+        return Friedrichshain;
+    }
+  }
+
   return (
     <div className="card-container" key={props.property.id}>
-      <div className="top-container">
-        <div className="flex">
-          <h2>{props.property.neighbourhood_group_cleansed}</h2>
-          <div className="edit-buttons">
-            <ion-icon
-              className="ion-icon"
-              name="ios-create"
-              onClick={editProperty}
-            ></ion-icon>
-            <ion-icon
-              className="ion-icon"
-              name="ios-trash"
-              onClick={deleteProperty}
-            ></ion-icon>
+      <div className='image-card'><img src={setImage(props.property.neighbourhood_group_cleansed)}/></div>
+      <div className='top-container'>
+        <div className='flex'>
+          <h2>{setName(props.property.neighbourhood_group_cleansed)}</h2>
+          <div className='edit-buttons'>
+            <ion-icon className='ion-icon' name="ios-create" onClick={editProperty}></ion-icon>
+            <ion-icon className='ion-icon'  name="ios-trash" onClick={deleteProperty}></ion-icon>
           </div>
         </div>
         <div className="property-info">
